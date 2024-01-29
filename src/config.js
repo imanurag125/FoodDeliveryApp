2e15ececc387de7e43436a51033bbeead811f203
Anurag Ranjan <94666780+imanurag125@users.noreply.github.com>
2024-01-29 13:42:34 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const IMG_CDN_URL = `https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/`;
 
 export const swiggyUrl =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5941&lng=85.1376&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5941&lng=85.1376&page_type=DESKTOP_WEB_LISTING";
 
 export const FETCH_MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.5940947&lng=85.1375645&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.5940947&lng=85.1375645&restaurantId=";
 
 //
 
